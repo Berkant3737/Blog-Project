7d7ceddcfee5bd3a9c76865ba66b71b512907716
Berkant <48642168+Berkant3737@users.noreply.github.com>
2023-05-29 02:36:53 +0300

    genel düzenleme

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -4,7 +4,7 @@ function Project() {
   return (
     <div className="container project">
       <h1 className="text-center">Projelerim</h1>
-      <div class="row justify-content-center mt-5">
+      <div class="row justify-content-center mt-5 mb-5">
         <div className="col-md-8">
           <div className="row g-4">
           {ProjectJson.map((project) => {
@@ -17,10 +17,10 @@ function Project() {
                     <h4 className="card-title">{project.title} </h4>
                     <p className="card-text">{project.description}</p>
                     <div className="d-flex justify-content-around">
-                      <a href={project.github} className="btn btn-primary">
+                      <a href={project.github} className="project-button btn btn-outline">
                         Github
                       </a>
-                      <a href={project.liveDemo} className="btn btn-primary">
+                      <a href={project.liveDemo} className="project-button btn btn-outline">
                         Canlı Demo
                       </a>
                     </div>
