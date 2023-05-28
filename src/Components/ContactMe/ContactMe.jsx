a010b1ec960c9a13a2bf783f18ec1316c8c6f60f
Berkant <48642168+Berkant3737@users.noreply.github.com>
2023-05-29 00:35:56 +0300

    Projelerim kısmı bitti

---
+++ src/Components/ContactMe/ContactMe.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function ContactMe() {
+  return (
+    <div className='container'>
+        <div className="row justify-content-center">
+            
+        </div>
+    </div>
+  )
+}
+
+export default ContactMe
